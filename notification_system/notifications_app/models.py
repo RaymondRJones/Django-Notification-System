2338ebdd6ab07c12c32a859d398d70bdf7b62612
RaymondJones <Rayjones2170@gmail.com>
2023-08-14 17:13:15 -0500

    Create a notification app, connect it to SendGrid API

--- notification_system/notifications_app/models.py
+++ notification_system/notifications_app/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 class User(models.Model):
+    class Meta:
+        app_label = 'notifications_app'
     name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
     group = models.CharField(max_length=50)  # Assuming group as a char field, could be a ForeignKey if it's another model.
-
+    def __str__(self):
+        return self.name
 class Message(models.Model):
+    class Meta:
+        app_label = 'notifications_app'
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     is_sent = models.BooleanField(default=False)
@@ -17,3 +22,5 @@ class Message(models.Model):
         # Add other types as needed
     )
     type = models.CharField(max_length=5, choices=TYPE_CHOICES)
+    def __str__(self):
+        return f"Message for user {self.user.name}"
---
+++ notification_system/notifications_app/views.py
@@ -0,0 +1,135 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import send_mail
+from .models import User, Message
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import send_mail
+from .models import User, Message
+import os
+from dotenv import load_dotenv
+load_dotenv()
+@csrf_exempt  # Note: This disables CSRF protection for the view. In production, a more secure method should be used.
+def send_notification(request):
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        username = request.POST.get('name')
+        message_type = request.POST.get('type')
+
+        try:
+            if user_id:
+                user = User.objects.get(pk=user_id)
+            elif username:
+                user = User.objects.get(name=username)
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Please provide a user_id or username'})
+            print("Gets the name")
+            message = Message.objects.filter(type=message_type).first()
+            if not message:
+                return JsonResponse({'status': 'error', 'message': 'Message not found'})
+            print("Gets the message")
+            msg = "<strong> " + message.content + "</strong>"
+            message_to_send = Mail(
+                from_email='ray@raymondjones.dev',
+                to_emails=user.email,
+                subject='Part 2 - Sending with Twilio SendGrid is Fun',
+                html_content=msg)
+            print(os.environ.get('SENDGRID_API_KEY'))
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message_to_send)
+            print("Response", response)
+            message.is_sent = True
+            message.save()
+
+            return JsonResponse({'status': 'success', 'message': 'Notification sent'})
+
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+    return JsonResponse({'status': 'error', 'message': 'Only POST method is allowed'})
+
+def send_notification_now(request):
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        username = request.POST.get('name')
+        message_type = request.POST.get('type')
+
+        try:
+            if user_id:
+                user = User.objects.get(pk=user_id)
+            elif username:
+                user = User.objects.get(name=username)
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Please provide a user_id or username'})
+            print("Gets the name")
+            message = Message.objects.filter(type=message_type).first()
+            if not message:
+                return JsonResponse({'status': 'error', 'message': 'Message not found'})
+            print("Gets the message")
+            msg = "<strong> " + message.content + "</strong>"
+            message_to_send = Mail(
+                from_email='ray@raymondjones.dev',
+                to_emails=user.email,
+                subject='Part 2 - Sending with Twilio SendGrid is Fun',
+                html_content=msg)
+            print(os.environ.get('SENDGRID_API_KEY'))
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message_to_send)
+            print("Response", response)
+            message.is_sent = True
+            message.save()
+
+            return JsonResponse({'status': 'success', 'message': 'Notification sent'})
+
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+    return JsonResponse({'status': 'error', 'message': 'Only POST method is allowed'})
+
+
+
+
+"""
+@csrf_exempt  # Note: This disables CSRF protection for the view. In production, a more secure method should be used.
+def send_notification_api(request):
+    if request.method == 'POST':
+        username = request.POST.get('name')
+        message_type = request.POST.get('type')
+
+        try:
+            if username:
+                user = User.objects.get(name=username)
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Please provide a user_id or username'})
+
+            message = Message.objects.filter(type=message_type).first()
+
+            if not message:
+                return JsonResponse({'status': 'error', 'message': 'Message not found'})
+
+            message = Mail(
+                    from_email='sender@example.com',
+                    to_emails=user.email,
+                    subject='Notification',
+                    plain_text_content=message.content)
+            try:
+                sg = SendGridAPIClient('YOUR_SENDGRID_API_KEY')  # Again, consider securely managing this API key.
+                response = sg.send(message)
+                # Check the response, etc. (not shown here)
+                return JsonResponse({'status': 'success', 'message': 'Notification sent'})
+
+            except Exception as e:
+                return JsonResponse({'status': 'error', 'message': str(e)})
+
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+    return JsonResponse({'status': 'error', 'message': 'Only POST method is allowed'})
+
+
+"""
+
